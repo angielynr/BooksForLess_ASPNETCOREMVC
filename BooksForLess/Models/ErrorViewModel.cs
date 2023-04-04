c2e197e5013dcea786d45873ff827166c81a479b
Angielyn Rivera <2194059@cognizant.com>
2023-04-04 17:26:23 +0800

    Add project files.

---
+++ BooksForLess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooksForLess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
