8b32507773cacf0da692712c9d9c42fd28690e1e
Angielyn Rivera <2194059@cognizant.com>
2023-04-05 13:56:30 +0800

    Initial Commit

---
+++ BooksForLess.Repository/DataContext/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BooksForLess.Repository.Entity;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BooksForLess.Data.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
+        
+        }
+
+        public DbSet<Categories> categories { get; set; }
+    }
+}
