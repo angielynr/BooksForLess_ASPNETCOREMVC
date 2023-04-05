3196583e6dc0bd79547b7f3efff8f517e393f116
Angielyn Rivera <2194059@cognizant.com>
2023-04-05 14:15:03 +0800

    Added categories controller & view

---
+++ BooksForLess/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BooksForLess.API.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult GetAllCategories()
+        {
+            return View();
+        }
+    }
+}
